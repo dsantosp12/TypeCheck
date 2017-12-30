21da7fb7d33d8ae412081ba2692dcf9e5f941cf5
Daniel Santos <dsantosp12@gmail.com>
2017-12-30 04:56:48 -0500

    Implemented type_check function

--- type_check/type_check.py
+++ type_check/type_check.py
@@ -9,13 +9,42 @@
 
 
 from functools import wraps
+import types
 
 
-def __type_check(*args, **kwargs):
+def __type_check(*deco_args, **deco_kwargs):
     def type_check_wrapper(f):
+        if not isinstance(f, types.FunctionType):
+            raise TypeError(
+                "This decorator is intended to functions or methods")
+
         @wraps(f)
-        def wrapper(*args, **kwargs):
-            raise TypeError("Testing")
+        def wrapper(*f_args, **f_kwargs):
+            # Path for keyword arguments
+            if len(deco_kwargs) > 0:
+                if not len(deco_kwargs) == len(f_args):
+                    raise IndexError("The number of argument in the decorator "
+                                     "don't match the function")
+                else:
+                    idx = 0
+                    for key in deco_kwargs:
+                        value = deco_kwargs[key]
+                        if not isinstance(f_args[idx], value):
+                            raise TypeError(
+                                "Argument {} is not of type {}".format(
+                                    key, value))
+                        idx += 1
+            else:  # Path for regular arguments
+                if not len(deco_args) == len(f_args):
+                    raise IndexError("The number of argument in the decorator "
+                                     "don't match the function")
+                idx = 0
+                for arg in deco_args:
+                    if not isinstance(f_args[idx], arg):
+                        raise TypeError(
+                            "Argument {} is not of type {}".format(idx+1, arg))
+                    idx += 1
+            return f(*f_args, **f_kwargs)
         return wrapper
     return type_check_wrapper
 
